2db749baab1e1db9e6122fab83a2c8125eac7712
Matt Billock <mattbillock@mattbillock.com>
2020-03-02 06:00:52 -0600

    Functional code

--- thundra_test/thundra-test/tests/unit/test-handler.js
+++ thundra_test/thundra-test/tests/unit/test-handler.js
@@ -35,3 +35,19 @@ describe('ChaosTests index', function () {
     });
 });
 
+describe.skip('SpanListener index', function () {
+    it('verifies successful response', async () => {
+        
+        const result = await app.spanListenerLambdaHandler(event, context)
+
+        expect(result).to.be.an('object');
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.be.an('string');
+
+        let response = JSON.parse(result.body);
+
+        expect(response).to.be.an('object');
+        expect(response.message).to.be.equal("Normal operation succeeded!");
+    });
+})
+
